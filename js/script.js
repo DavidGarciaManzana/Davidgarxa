3177462f88472d4e941c715942b9f49697689c80
David Garcia <Davidgarciama@outlook.com>
2021-08-04 07:43:48 -0500

    First version of the webpage

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const
+    input = document.getElementById("email"),
+    form = document.getElementById("form")
+
+
+const validateEmail = (e) => {
+    e.preventDefault()
+    let email = input.value
+    let regexp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i
+
+    if(!email.length>0 || !regexp.test(email)){
+        console.log("email invalido")
+        console.log(email)
+        console.log(email.length)
+
+        console.log(!email.length>0)
+        console.log(!regexp.test(email))
+
+
+        document.querySelector(".error").style.display="block"
+        document.querySelector(".span").style.display="block"
+        return false
+    } else {
+        console.log("email valido")
+        document.querySelector(".error").style.display="none"
+        document.querySelector(".span").style.display="none"
+        form.submit()
+        input.value = ""
+    }
+
+}
+
+form.addEventListener("submit", validateEmail)
